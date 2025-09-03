794f73790e2095789a3d4631fb4356222de28c86
Nilton Sousa <nsousa@zak.app>
2025-09-03 20:40:26 -0300

    feat: create account coin router

---
+++ internal/handlers/api.go
@@ -0,0 +1,15 @@
+package handlers
+
+import (
+	middleware "github.com/NiltonSousa/goapi/internal/middleware"
+	"github.com/go-chi/chi"
+	chimiddleware "github.com/go-chi/chi/middleware"
+)
+
+func Handler(r *chi.Mux) {
+	r.Use(chimiddleware.StripSlashes)
+	r.Route("/account", func(router chi.Router) {
+		router.Use(middleware.Authorization)
+		router.Get("/coins", GetCoinBalance)
+	})
+}
